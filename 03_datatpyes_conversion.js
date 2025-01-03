f3e56137550f57ac741ccdf05d4981036c4e50a7
Aditya Shekhar <adityashekhar862@gmail.com>
2025-01-03 21:07:01 +0530

    Add files via upload

---
+++ 03_datatpyes_conversion.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+/console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+
+
+let isLoggedIN = 1
+
+let boolenIsLoggedIN = Boolean(isLoggedIN);
+console.log(boolenIsLoggedIN);
+
+// 0, "", null, undefined, NaN -> false
+// rest all true
+// "aditya" -> true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
