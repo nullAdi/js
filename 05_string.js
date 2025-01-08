be0ef18619fde144b852de3258fde6bf6310e9db
Aditya Shekhar <adityashekhar862@gmail.com>
2025-01-08 19:38:28 +0530

    Add files via upload

---
+++ 05_string.js
@@ -0,0 +1,49 @@
+const name = "gaurav"
+const repoCount = 6
+
+// console.log( name + repoCount + value);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('gaurav-dt-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// // console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('d'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const newStringOne = "  gaurav    "
+// console.log(newStringOne.trim());
+
+
+const url = "https:golu.com/golu%20choudhary"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('golu'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
